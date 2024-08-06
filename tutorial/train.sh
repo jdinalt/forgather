e4011890f4739505bf0c271d55ec9392da584892
Jason dinAlt <joedkloss@gmail.com>
2024-08-06 04:27:21 +0000

    Removed tutorial code; will rewrite

--- tutorial/train.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-torchrun --standalone --nproc-per-node gpu '../scripts/train_script.py' -w 'forgather_demo/whitelist.yaml' -I 'forgather_demo' -I '../templates' -I '../model_zoo'  -s '..' "${@}"
